52b2cc975b1433145c771a222566912dd320c393
InbalGam <inbalgam@post.bgu.ac.il>
2023-06-14 13:10:48 +0300

    added comments APIs

--- server/trips-api.js
+++ server/trips-api.js
@@ -240,4 +240,100 @@ tripsRouter.delete('/trips/:trip_id/activities/:activity_id', (req, res, next) =
 });
 
 
+// Comments
+
+// Get all comments-
+tripsRouter.get('/trips/:trip_id/activities/:activity_id/comments', (req, res, next) => {
+    query('select ac.* from activities a join activity_comments ac on a.id = ac.activity_id where a.id = $1 and a.trip_id = $2;', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different activity, this one does not have comments yet' });
+        }
+
+        res.status(200).json(results.rows);
+      });
+});
+
+
+// Post a new comment
+tripsRouter.post('/trips/:trip_id/activities/:activity_id/comments', (req, res, next) => {
+    const { comment } = req.body;
+
+    query('select * from activities where id = $1 and trip_id = $2;', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different activity or trip' });
+        }
+
+        const timestamp = new Date(Date.now());
+        query('insert into activity_comments (activity_id, user_id, comment, timestamp) values ($1, $2, $3, $4);', [req.params.activity_id, req.user.id, comment, timestamp],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Added comment' });
+            }
+        );
+    }
+    );
+});
+
+
+// Update a specific comment
+tripsRouter.put('/trips/:trip_id/activities/:activity_id/comments/:comment_id', (req, res, next) => {
+    const { comment } = req.body;
+
+    query('select * from activities where id = $1 and trip_id = $2;', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different activity or trip' });
+        }
+
+        const timestamp = new Date(Date.now());
+        query('update activity_comments set activity_id = $2, user_id = $3, comment = $4, timestamp = $5 where id = $1;', [req.params.comment_id, req.params.activity_id, req.user.id, comment, timestamp],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Updated comment' });
+            }
+        );
+    }
+    );
+});
+
+
+// Delete a specific comment
+tripsRouter.delete('/trips/:trip_id/activities/:activity_id/comments/:comment_id', (req, res, next) => {
+    query('select * from activities where id = $1 and trip_id = $2;', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different activity or trip' });
+        }
+
+        query('delete from activity_comments where id = $1 and activity_id = $2;', [req.params.comment_id, req.params.activity_id],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Deleted comment' });
+            }
+        );
+    }
+    );
+});
+
+
 module.exports = tripsRouter;
