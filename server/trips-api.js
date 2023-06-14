90c3d6b703f60f507b954b278d32fb07a0020c7e
InbalGam <inbalgam@post.bgu.ac.il>
2023-06-14 11:28:45 +0300

    added trips and activities APIs

--- server/trips-api.js
+++ server/trips-api.js
@@ -11,6 +11,19 @@ tripsRouter.use((req, res, next) => {
 });
 
 
+function isValidDate(dateString) {
+    return !isNaN(Date.parse(dateString));
+}
+
+
+function validateHhMm(inputField) {
+    const isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField);
+    return isValid;
+}
+
+
+// Trips
+
 // Get all trips-
 tripsRouter.get('/trips', (req, res, next) => {
     query('select * from trips', (error, results) => {
@@ -21,6 +34,210 @@ tripsRouter.get('/trips', (req, res, next) => {
       });
 });
 
+
+// Post a new trip
+tripsRouter.post('/trips', (req, res, next) => {
+    const { country, city, start_date, end_date } = req.body;
+
+    if (!isValidDate(start_date) || !isValidDate(end_date)) {
+        return res.status(400).json({msg: 'The start date and end date must be in the correct format'});
+    }
+
+    query('insert into trips (country, city, start_date, end_date) values ($1, $2, $3, $4);', [country, city, start_date, end_date], 
+        (error, results) => {
+            if (error) {
+                return res.status(500);
+            }
+            res.status(200).json({msg: 'Added trip'});
+        }
+    );
+});
+
+
+// Get a specific trip
+tripsRouter.get('/trips/:trip_id', (req, res, next) => {
+    query('select * from trips where id = $1', [req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({msg: 'Please choose a different trip, this one is not in the system'});
+        }
+
+        res.status(200).json(results.rows[0]);
+      });
+});
+
+
+// Update a specific trip
+tripsRouter.put('/trips/:trip_id', (req, res, next) => {
+    const { country, city, start_date, end_date } = req.body;
+
+    if (!isValidDate(start_date) || !isValidDate(end_date)) {
+        return res.status(400).json({ msg: 'The start date and end date must be in the correct format' });
+    }
+
+    query('select * from trips where id = $1', [req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different trip, this one is not in the system' });
+        }
+
+        query('update trips set country = $2, city = $3, start_date = $4, end_date = $5 where id = $1;', [req.params.trip_id, country, city, start_date, end_date],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Updated trip' });
+            }
+        );
+    }
+    );
+});
+
+
+// Delete a specific trip
+tripsRouter.delete('/trips/:trip_id', (req, res, next) => {
+    query('select * from trips where id = $1', [req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different trip, this one is not in the system' });
+        }
+
+        query('delete from trips where id = $1;', [req.params.trip_id],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Deleted trip' });
+            }
+        );
+    }
+    );
+});
+
+// Activities
+
+// Get all activities-
+tripsRouter.get('/trips/:trip_id/activities', (req, res, next) => {
+    query('select * from activities where trip_id = $1', [req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different trip, this one does not have activities yet' });
+        }
+
+        res.status(200).json(results.rows);
+      });
+});
+
+
+// Post a new activity
+tripsRouter.post('/trips/:trip_id/activities', (req, res, next) => {
+    const { date, activity_name, address, url, start_time, end_time, user_notes } = req.body;
+
+    if(!validateHhMm(start_time) || !validateHhMm(end_time)) {
+        return res.status(400).json({ msg: 'The start time and end time must be in the correct format' });
+    }
+
+    if (!isValidDate(date)) {
+        return res.status(400).json({msg: 'The date must be in the correct format'});
+    }
+
+    query('insert into activities (date, trip_id, activity_name, address, url, start_time, end_time, user_id, user_notes) values ($1, $2, $3, $4, $5, $6, $7, $8, $9);', 
+        [date, req.params.trip_id, activity_name, address, url, start_time, end_time, req.user.id, user_notes], 
+        (error, results) => {
+            if (error) {
+                return res.status(500);
+            }
+            res.status(200).json({msg: 'Added activity'});
+        }
+    );
+});
+
+
+// Get a specific activity
+tripsRouter.get('/trips/:trip_id/activities/:activity_id', (req, res, next) => {
+    query('select * from activities where id = $1 and trip_id = $2', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({msg: 'Please choose a different activity, this one is not in the system'});
+        }
+
+        res.status(200).json(results.rows[0]);
+      });
+});
+
+
+// Update a specific activity
+tripsRouter.put('/trips/:trip_id/activities/:activity_id', (req, res, next) => {
+    const { date, activity_name, address, url, start_time, end_time, user_notes } = req.body;
+
+    if(!validateHhMm(start_time) || !validateHhMm(end_time)) {
+        return res.status(400).json({ msg: 'The start time and end time must be in the correct format' });
+    }
+
+    if (!isValidDate(date)) {
+        return res.status(400).json({msg: 'The date must be in the correct format'});
+    }
+
+    query('select * from activities where id = $1 and trip_id = $2;', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different activity, this one is not in the system' });
+        }
+
+        query('update activities set date = $2, trip_id = $3, activity_name = $4, address = $5, url = $6, start_time = $7, end_time = $8, user_id = $9, user_notes = $10 where id = $1 and trip_id = $3;', 
+        [req.params.activity_id, date, req.params.trip_id, activity_name, address, url, start_time, end_time, req.user.id, user_notes],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Updated activity' });
+            }
+        );
+    }
+    );
+});
+
+
+// Delete a specific activity
+tripsRouter.delete('/trips/:trip_id/activities/:activity_id', (req, res, next) => {
+    query('select * from activities where id = $1 and trip_id = $2;', [req.params.activity_id, req.params.trip_id], (error, results) => {
+        if (error) {
+            return res.status(500);
+        }
+
+        if (results.rows.length === 0) {
+            return res.status(400).json({ msg: 'Please choose a different activity, this one is not in the system' });
+        }
+
+        query('delete from activities where id = $1 and trip_id = $2;', [req.params.activity_id, req.params.trip_id],
+            (error, results) => {
+                if (error) {
+                    return res.status(500);
+                }
+                res.status(200).json({ msg: 'Deleted activity' });
+            }
+        );
+    }
+    );
+});
 
 
 module.exports = tripsRouter;
