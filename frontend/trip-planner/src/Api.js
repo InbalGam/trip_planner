2abbb6ac625e23d882ccc412c6c958b95e43a2d5
InbalGam <inbalgam@post.bgu.ac.il>
2023-06-26 12:39:28 +0300

    added API fetch

--- frontend/trip-planner/src/Api.js
+++ frontend/trip-planner/src/Api.js
@@ -8,7 +8,7 @@ async function login(username, password) {
     const url = `${baseURL}/login`;
     const response = await fetch(url, {
         method: 'POST',
-        credentials: 'same-origin',
+        credentials: 'include',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({username, password})
     });
@@ -16,4 +16,29 @@ async function login(username, password) {
     return response.status === 200;
 };
 
-export {login};
+
+async function getTrips() {
+    const url = `${baseURL}/trips`;
+    const response = await fetch(url, {
+        method: 'GET',
+        credentials: 'include'
+    });
+
+    return response;
+};
+
+
+async function insertTrip(trip) {
+    const {country, city, start_date, end_date, emails} = trip;
+    const url = `${baseURL}/trips`;
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({country, city, start_date, end_date, emails})
+    });
+
+    return response;
+};
+
+export {login, getTrips, insertTrip};
