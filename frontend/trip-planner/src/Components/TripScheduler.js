be7292e72ae91c54bf97b5176571a0acdbc2764b
InbalGam <inbalgam@post.bgu.ac.il>
2023-06-27 13:10:24 +0300

    added trip activities view

--- frontend/trip-planner/src/Components/TripScheduler.js
+++ frontend/trip-planner/src/Components/TripScheduler.js
@@ -3,33 +3,52 @@ import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
-  DayView,
+  MonthView,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { useParams } from 'react-router-dom';
-
+import {getSpecificTrip, getActivities} from '../Api';
+import { useState, useEffect } from "react";
+import dateFormat, { masks } from "dateformat";
 
 
 function TripScheduler() {
     const { tripId } = useParams();
+    const [trip, setTrip] = useState({});
+    const [schedulerData, setSchedulerData] = useState([]);
+    const [currentDate, setCurrentDate] = useState('');
+        
 
-    const currentDate = '2018-11-01';
-    const schedulerData = [
-        { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-        { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-    ];
+
+    async function getTripInfo(id) {
+        const result = await getSpecificTrip(id);
+        const jsonData = await result.json();
+        setTrip(jsonData);
+    };
+
+
+    async function getTripActivities(id) {
+        const result = await getActivities(id);
+        const jsonData = await result.json();
+        const activities = jsonData.map(el => {
+            const newDate = dateFormat(new Date(el.date), "yyyy, mm, dd");
+            return {title: el.activity_name, startDate: new Date(newDate + ', ' + el.start_time), endDate: new Date(newDate + ', ' + el.end_time)}
+        });
+        setSchedulerData(activities);
+        const formattedDate = dateFormat(new Date(activities[0].startDate), "yyyy-mm-dd");
+        setCurrentDate(formattedDate.toString());
+    };
+
+    useEffect(() => {
+        getTripInfo(tripId);
+        getTripActivities(tripId);
+    }, []);
+
 
     return (<Paper>
-        <Scheduler
-            data={schedulerData}
-        >
-            <ViewState
-                currentDate={currentDate}
-            />
-            <DayView
-                startDayHour={9}
-                endDayHour={14}
-            />
+        <Scheduler data={schedulerData}>
+            <ViewState currentDate={currentDate} />
+            <MonthView />
             <Appointments />
         </Scheduler>
     </Paper>);
