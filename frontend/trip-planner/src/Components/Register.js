c23139a0aabf1fe82074555f1d305174ced7d10f
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-03 15:51:55 +0300

    fixed imports

--- frontend/trip-planner/src/Components/Register.js
+++ frontend/trip-planner/src/Components/Register.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import {register} from '../Api';
-import { useNavigate } from 'react-router-dom';
 import {validateEmail} from '../utils';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 
 
 function Register() {
