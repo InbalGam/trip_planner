208c1cc1c39cedf73c56263f87f37a04159e107e
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-04 14:38:37 +0300

    added form class

--- frontend/trip-planner/src/Components/Login.js
+++ frontend/trip-planner/src/Components/Login.js
@@ -36,22 +36,22 @@ function Login() {
     };
 
     return (
-        <div className="login_container">
-            <p className='messages'>{searchParams.get("logout") ? 'Succefully logged out' : ''}</p>
-            <p className='messages'>{searchParams.get("register") ? 'Succefully registered, you can log in' : ''}</p>
-            <p className='loginHeadline'>Log in below</p>
-            <form onSubmit={submitLogin}>
+        <div className={styles.login_container}>
+            <p className={styles.messages}>{searchParams.get("logout") ? 'Succefully logged out' : ''}</p>
+            <p className={styles.messages}>{searchParams.get("register") ? 'Succefully registered, you can log in' : ''}</p>
+            <p className={styles.loginHeadline}>Log in below</p>
+            <form onSubmit={submitLogin} className={styles.loginForm}>
                 <label for='username'>Username</label>
                 <input id='username' type='text' name='username' value={username} placeholder={'username'} onChange={handleUsernameChange} />
                 <label for='password'>Password</label>
                 <input id='password' type='password' name='password' value={password} placeholder={'password'} onChange={handlePasswordChange} />
                 <button type="submit" value="Submit">Submit</button>
             </form>
-            <div className='authStatus'>
+            <div className={styles.authStatus}>
                 {authFailed ? 'Username or Password are incorrect, try again' : ''}
             </div>
-            <p className='registrationHeadline'>Not registered yet?</p>
-            <Link to='/register' className='registrationLink' >Register here</Link>
+            <p className={styles.registrationHeadline}>Not registered yet?</p>
+            <Link to='/register' className={styles.registrationLink} >Register here</Link>
         </div>
     );
 };
