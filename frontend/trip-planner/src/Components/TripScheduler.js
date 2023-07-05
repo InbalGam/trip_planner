71e678fcad867a430433c910cc8c4d00faef05e6
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-05 15:03:51 +0300

    added activity type

--- frontend/trip-planner/src/Components/TripScheduler.js
+++ frontend/trip-planner/src/Components/TripScheduler.js
@@ -56,7 +56,7 @@ function TripScheduler() {
                 if (jsonData.length > 0) {
                     const activities = jsonData.map(el => {
                         const newDate = dateFormat(new Date(el.date), "yyyy, mm, dd");
-                        return { id: el.id, title: el.activity_name, startDate: new Date(newDate + ', ' + el.start_time), endDate: new Date(newDate + ', ' + el.end_time) }
+                        return { id: el.id, title: el.activity_name, startDate: new Date(newDate + ', ' + el.start_time), endDate: new Date(newDate + ', ' + el.end_time), type: el.type }
                     });
                     setSchedulerData(activities);
                 } else {
