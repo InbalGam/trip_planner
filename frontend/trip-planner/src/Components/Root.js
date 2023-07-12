0127d1c26db0067ed3a180749b1537b552e4d034
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-12 10:57:10 +0300

    added icon

--- frontend/trip-planner/src/Components/Root.js
+++ frontend/trip-planner/src/Components/Root.js
@@ -1,12 +1,13 @@
 import {  Outlet, NavLink } from "react-router-dom";
 import styles from './Styles/Root.css';
+import LuggageIcon from '@mui/icons-material/Luggage';
 
 
 function Root() {
     return (
         <div>
             <div className='nav-bar'>
-                <p>The Trip Planner</p>
+                <p><LuggageIcon className="luggageIcon"/> The Trip Planner</p>
                 <div className='nav-links'>
                     <NavLink to='/trips' className='tripsLink'>Trips</NavLink>
                     <NavLink to='/logout' className='logoutLink'>Log out</NavLink>
