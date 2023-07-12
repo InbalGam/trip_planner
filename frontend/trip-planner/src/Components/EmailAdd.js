83e5370501101ec08991e038fef0a0957aee0b17
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-12 10:39:51 +0300

    removed comments

--- frontend/trip-planner/src/Components/EmailAdd.js
+++ frontend/trip-planner/src/Components/EmailAdd.js
@@ -3,7 +3,6 @@ import { ReactMultiEmail, isEmail } from 'react-multi-email';
 import 'react-multi-email/dist/style.css';
 
 function Email(props) {
-  //const [emails, setEmails] = useState([]);
   const [focused, setFocused] = useState(false);
 
   return (
@@ -14,8 +13,6 @@ function Email(props) {
         emails={props.emails}
         onChange={(_emails) => {props.setEmails(_emails);}}
         autoFocus={true}
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(false)}
         getLabel={(email, index, removeEmail) => {
           return (
             <div data-tag key={index}>
@@ -28,9 +25,6 @@ function Email(props) {
         }}
       />
       <br />
-      {/* <h4>react-multi-email value</h4> */}
-      {/* <h3>Focused: {focused ? 'true' : 'false'}</h3> */}
-      {/* <p>{emails.join(', ') || 'empty'}</p> */}
     </form>
   );
 };
