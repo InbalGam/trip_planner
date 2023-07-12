e780fa06439030a8ce12862683d40e84922727d4
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-12 10:45:00 +0300

    removed not necessary params

--- frontend/trip-planner/src/Components/Logout.js
+++ frontend/trip-planner/src/Components/Logout.js
@@ -4,13 +4,11 @@ import { useNavigate } from 'react-router-dom';
 
 
 function Logout() {
-    const [logOut, setLogOut] = useState(false);
     const navigate = useNavigate();
 
     async function loggingOut() {
         const result = await logout();
         if (result.status === 200) {
-            setLogOut(true);
             navigate('/login?logout=1');
         }
     };
@@ -18,12 +16,6 @@ function Logout() {
     useEffect(() => {
         loggingOut();
     }, []);
-
-    return (
-        <div>
-            <p>{logOut ? 'Succefully logged out' : ''}</p>
-        </div>
-    );
 };
 
 export default Logout;
