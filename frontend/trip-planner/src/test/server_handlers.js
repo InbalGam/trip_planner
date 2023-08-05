101f44ee2eaf021b21fba53f0ecddc33df147d87
InbalGam <inbalgam@post.bgu.ac.il>
2023-08-05 18:20:34 +0300

    fix

--- frontend/trip-planner/src/test/server_handlers.js
+++ frontend/trip-planner/src/test/server_handlers.js
@@ -24,7 +24,7 @@ const handlers = [
             "start_date": "2023-07-14T21:00:00.000Z",
             "end_date": "2023-07-25T21:00:00.000Z",
             "created_by": 17,
-            "photo": "https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sAaw_FcKCJIM0IDU3k2WCOrEtmnODR-VfTKUBbat6S2vkZR3LlLyCpgAZdP1KYR_AJfm4W1h23REHGLHE9PdsfIcWWM9YHIjkY5HQJrn_bHmsU8ATz3GBIAnoPhUlqiQ9U4Ev8zpd1EVDQ7pe7iZu8ba5sGHYPMvfb1mVv3EQuMROxyIsX9ir&3u720&5m1&2e1&callback=none&key=AIzaSyCHCHNhjtJ64EuJ23r9UbBwmsNai3NNSeM&token=16775",
+            "photo": null,
             "isCreatedByMe": 1
         },
         {
