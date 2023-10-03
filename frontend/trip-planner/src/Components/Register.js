e097895a250928c5634f23474dd56405e88b7453
InbalGam <inbalgam@post.bgu.ac.il>
2023-10-03 14:15:27 +0300

    added a new visual for register page

--- frontend/trip-planner/src/Components/Register.js
+++ frontend/trip-planner/src/Components/Register.js
@@ -2,17 +2,35 @@ import { useState } from "react";
 import {register} from '../Api';
 import {validateEmail} from '../utils';
 import { useNavigate} from 'react-router-dom';
-import styles from './Styles/Register.css';
+//import styles from './Styles/Register.css';
 import ClipLoader from 'react-spinners/ClipLoader';
+import tw from "twin.macro";
+import {css} from "styled-components/macro"; //eslint-disable-line
+import Alert from '@mui/material/Alert';
+import { ReactComponent as SignUpIcon } from "feather-icons/dist/icons/user-plus.svg";
+import googleIconImageSrc from "./Styles/images/google-icon.png";
+import * as rst from './Styles/RegisterStyles';
+import {baseURL} from '../apiKey';
+import signupImg from './Styles/images/clarisse-meyer-d6pLNFVZt_4-unsplash.jpg';
 
 
-function Register() {
+function Register({
+    socialButtons = [
+        {
+          iconImageSrc: googleIconImageSrc,
+          text: "Sign Up With Google",
+          url: `${baseURL}/login/google`
+        }
+      ],
+    submitButtonText = "Sign Up",
+    SubmitButtonIcon = SignUpIcon,
+    signInUrl='/login',
+    illustrationImageSrc = signupImg
+}) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [nickname, setNickname] = useState('');
     const [validUsername, setValidUsername] = useState(true);
-    const [validPassword, setValidPassword] = useState(true);
-    const [validNickname, setValidNickname] = useState(true);
     const [registerAuth, setRegisterAuth] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const[msg, setMsg] = useState('');
@@ -20,6 +38,7 @@ function Register() {
 
     function handleUsernameChange(e) {
         setUsername(e.target.value);
+        setValidUsername(validateEmail(e.target.value));
     };
 
     function handlePasswordChange(e) {
@@ -33,11 +52,8 @@ function Register() {
 
     async function submitForm(e) {
         e.preventDefault();
-        setValidPassword(password.length >= 8);
-        setValidNickname(nickname.length >= 3);
-        setValidUsername(validateEmail(username));
         
-        if ((password.length >= 8) && (nickname.length >= 3) && (validateEmail(username))) {
+        if ((password.length >= 8) && (nickname.length >= 3) && validUsername) {
             try {
                 setIsLoading(true);
                 const result = await register(username, password, nickname);
@@ -47,13 +63,12 @@ function Register() {
                     setUsername('');
                     setPassword('');
                     setNickname('');
-                    setValidNickname(true);
-                    setValidPassword(true);
                     setValidUsername(true);
                     navigate('/login?register=1');
                     setIsLoading(false);
                 } else {
                     setRegisterAuth(true);
+                    setMsg(jsonData.msg);
                     setIsLoading(false);
                 }
             } catch (e) {
@@ -64,24 +79,56 @@ function Register() {
 
 
     return (
-        <div className="register_container">
-            <h2>Welcome to The Trip Planner</h2>
-            <h3>You are minutes away from starting to plan your dream vacation!</h3>
-            <form onSubmit={submitForm} className='registerForm'>
-                <label htmlFor='username'>Email-</label>
-                <input id='username' type='text' name='username' value={username} placeholder={'Enter your email here'} onChange={handleUsernameChange} />
-                {validUsername ? '' : 'The username needs to be a valid email'}
-                <label htmlFor='password'>Password-</label>
-                <input id='password' type='password' name='password' value={password} placeholder={'Enter your password here'} onChange={handlePasswordChange} />
-                {validPassword ? '' : 'Your password must be at least 8 characters'}
-                <label htmlFor='nickname'>Nickname-</label>
-                <input id='nickname' type='text' name='nickname' value={nickname} placeholder={'Enter your nickname here'} onChange={handleNicknameChange} />
-                {validNickname ? '' : 'Your nickname must be at least 3 characters'}
-                {isLoading ? <ClipLoader color={'#3c0c21'} size={150} className='submitLoader'/> : <button type="submit" value="Submit" className="submitButton">Register</button>}
-                {registerAuth ? 'Could not register' : ''}
-                {msg ? msg : ''}
-            </form>
-        </div>
+        <rst.Container>
+            <rst.Content>
+                <rst.MainContainer>
+                    <rst.MainContent>
+                        <rst.Heading>{'Sign Up To The Trip Planner'}</rst.Heading>
+                        <rst.FormContainer>
+                            <rst.SocialButtonsContainer>
+                                {socialButtons.map((socialButton, index) => (
+                                    <rst.SocialButton key={index} href={socialButton.url}>
+                                        <span className="iconContainer">
+                                            <img src={socialButton.iconImageSrc} className="icon" alt="" />
+                                        </span>
+                                        <span className="text">{socialButton.text}</span>
+                                    </rst.SocialButton>
+                                ))}
+                            </rst.SocialButtonsContainer>
+                            <rst.DividerTextContainer>
+                                <rst.DividerText>Or Sign up with your e-mail</rst.DividerText>
+                            </rst.DividerTextContainer>
+                            <rst.Form onSubmit={submitForm}>
+                                <rst.Input type="email" placeholder="Email" value={username} onChange={handleUsernameChange} />
+                                <p tw="mt-4">{username && !validUsername ? <Alert severity="warning">The username needs to be a valid email</Alert> : ''}</p>
+                                <rst.Input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+                                <p tw="mt-4">{password && (password.length < 8) ? <Alert severity="warning">Password is less than 8 characters</Alert> : ''}</p>
+                                <rst.Input type="text" placeholder="Username" value={nickname} onChange={handleNicknameChange} />
+                                <p tw="mt-4">{nickname && (nickname.length < 3) ? <Alert severity="warning">Nickname is less than 3 characters</Alert> : ''}</p>
+                                <rst.LoaderContainer>
+                                    {isLoading ? <ClipLoader color={'#3c0c21'} size={150} className='submitLoader' /> :
+                                        <rst.SubmitButton type="submit">
+                                            <SubmitButtonIcon className="icon" />
+                                            <span className="text">{submitButtonText}</span>
+                                        </rst.SubmitButton>}
+                                </rst.LoaderContainer>
+                                <p tw="mt-4">{msg ? <Alert severity="warning">{msg}</Alert> : ''}</p>
+                            </rst.Form>
+                            <p tw="mt-4">{registerAuth ? <Alert severity="warning">Could not register</Alert> : ''}</p>
+                            <p tw="mt-8 text-sm text-gray-600 text-center">
+                                Already have an account?{" "}
+                                <a href={signInUrl} tw="border-b border-gray-500 border-dotted">
+                                    Sign In
+                                </a>
+                            </p>
+                        </rst.FormContainer>
+                    </rst.MainContent>
+                </rst.MainContainer>
+                <rst.IllustrationContainer>
+                    <rst.IllustrationImage imageSrc={illustrationImageSrc} aria-label="Photo by Clarisse Meyer"/>
+                </rst.IllustrationContainer>
+            </rst.Content>
+        </rst.Container>
     );
 };
 
